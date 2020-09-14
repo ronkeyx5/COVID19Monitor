6cf0ce5de051244a546701c1e7220c807fcb891d
Juan Ricardo <ronkeyx5@hotmail.com>
2020-09-14 17:33:16 -0500

    Bar & Pie Charts
    No como lo había pensado

--- app/src/main/java/com/example/covid199monitor/BarChart.java
+++ app/src/main/java/com/example/covid199monitor/BarChart.java
@@ -13,59 +13,35 @@ import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Cartesian;
-import com.anychart.charts.Cartesian3d;
 import com.anychart.charts.Pie;
-import com.anychart.core.cartesian.series.Bar3d;
-import com.anychart.core.lineargauge.pointers.Bar;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PieChart extends AppCompatActivity {
-
-    AnyChartView pieChart;
+public class BarChart extends AppCompatActivity {
+    AnyChartView barChart;
 
     TextView text;
-    Button alt;
-
-    List<Estado> estados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pie_chart);
+        setContentView(R.layout.activity_bar_chart);
 
-        pieChart = findViewById(R.id.pieChart);
+        barChart = findViewById(R.id.barChart);
 
         Intent i = getIntent();
-        estados = (List<Estado>) i.getSerializableExtra("estados");
-        final int type = Integer.parseInt(i.getStringExtra("type"));
+        List<Estado> estados = (List<Estado>) i.getSerializableExtra("estados");
+        int type = Integer.parseInt(i.getStringExtra("type"));
 
         text = findViewById(R.id.title);
-        alt = findViewById(R.id.alt);
 
-        alt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ViewBarChartC(Integer.toString(type));
-            }
-        });
-
-        PieChartShow(type, estados);
+        BarChartShow(type, estados);
     }
 
-    private void ViewBarChartC(String q) {
-        Intent i = new Intent(this, BarChart.class);
-        i.putExtra("type", q);
-        i.putExtra("estados", (Serializable)estados);
-
-        startActivity(i);
-    }
-
-    private void PieChartShow(int type, List<Estado> estados) {
+    private void BarChartShow(int type, List<Estado> estados) {
         if(type==1) {
-            Pie pie = AnyChart.pie();
+            Cartesian bar = AnyChart.bar();
 
             List<DataEntry> data = new ArrayList<>();
 
@@ -75,12 +51,12 @@ public class PieChart extends AppCompatActivity {
 
             text.setText("Casos Positivos");
 
-            pie.data(data);
+            bar.data(data);
 
-            pieChart.setChart(pie);
+            barChart.setChart(bar);
         }
         else if(type==2) {
-            Pie pie = AnyChart.pie();
+            Cartesian bar = AnyChart.bar();
 
             List<DataEntry> data = new ArrayList<>();
 
@@ -90,9 +66,9 @@ public class PieChart extends AppCompatActivity {
 
             text.setText("Fallecimientos");
 
-            pie.data(data);
+            bar.data(data);
 
-            pieChart.setChart(pie);
+            barChart.setChart(bar);
         }
     }
 }
